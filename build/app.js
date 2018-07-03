b81245ec0131e7fbfccf27413753e48ae99338ef
claudio <claudio.dev96@gmail.com>
2018-07-03 10:27:25 -0400

    Aplicacion de prueba en consola finalizada

--- build/app.js
+++ build/app.js
@@ -1,2 +1,5 @@
 "use strict";
+var date = new Date();
 console.log("app running in the console");
+console.log("Imprimiendo objeto date");
+console.log(date);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,5 @@
+var date = new Date();
 console.log("app running in the console")
+console.log("Imprimiendo objeto date")
+console.log(date)
+
